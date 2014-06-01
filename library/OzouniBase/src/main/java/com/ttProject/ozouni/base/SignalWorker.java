ddb13f614c40eabf857fc8ea0c3a6e12059547d1
taktod_win8 <poepoemix@hotmail.com>
2014-06-02 07:10:43 +0900

    signal動作用のオブジェクト作りかけ。

---
+++ library/OzouniBase/src/main/java/com/ttProject/ozouni/base/SignalWorker.java
@@ -0,0 +1,55 @@
+package com.ttProject.ozouni.base;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 一定時間ごとに処理をするbean
+ * @author taktod
+ */
+public class SignalWorker {
+	/** timer動作用executor */
+	private ScheduledExecutorService executor;
+	/** 動作間隔 */
+	private long interval = 1000; // デフォルトは1秒ごと
+	/** タイマー動作用future */
+	private ScheduledFuture<?> future = null;
+	/**
+	 * 動作させるのに利用するexecutorを設定する
+	 * @param executor
+	 */
+	public void setSignalExecutor(ScheduledExecutorService executor) {
+		this.executor = executor;
+		restartTimer();
+	}
+	/**
+	 * 動作間隔を設定(ミリ秒)
+	 * @param interval
+	 */
+	public void setInterval(long interval) {
+		this.interval = interval;
+		restartTimer();
+	}
+	/**
+	 * 保持データでタイマーを再開する
+	 */
+	private void restartTimer() {
+		if(executor == null) {
+			// 先にintervalが設定されることもあるため
+			return;
+		}
+		if(future != null) {
+			future.cancel(true); // 前の処理は破棄する。
+		}
+		future = executor.scheduleAtFixedRate(new Runnable() {
+			private long lastTime = 0;
+			@Override
+			public void run() {
+				long now = System.currentTimeMillis();
+				System.out.println("timerEvent:" + (now - lastTime));
+				lastTime = now;
+			}
+		}, interval, interval, TimeUnit.MILLISECONDS);
+	}
+}
---
+++ library/OzouniBase/src/test/java/com/ttProject/ozouni/base/test/SignalWorkerTest.java
@@ -0,0 +1,27 @@
+package com.ttProject.ozouni.base.test;
+
+import java.util.concurrent.Executors;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+import com.ttProject.ozouni.base.SignalWorker;
+
+/**
+ * signalWorkerの動作について確認するテスト
+ * @author taktod
+ */
+public class SignalWorkerTest {
+	/** ロガー */
+	private Logger logger = Logger.getLogger(SignalWorkerTest.class);
+//	@Test
+	public void test() throws Exception {
+		logger.info("テスト開始");
+		SignalWorker worker = new SignalWorker();
+		worker.setSignalExecutor(Executors.newSingleThreadScheduledExecutor());
+		Thread.sleep(10000);
+		worker.setInterval(2000);
+		Thread.sleep(10000);
+		logger.info("テスト終わり");
+	}
+}
