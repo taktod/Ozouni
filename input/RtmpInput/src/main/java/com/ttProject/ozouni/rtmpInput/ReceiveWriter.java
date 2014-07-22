3cf57be6800f313515acaceccab8f30bd4674043
todatakahiko <toda.takahiko@hpsys.co.jp>
2014-07-22 16:40:18 +0900

    実際のframeのpts値や更新情報は、frameを送るときにつける形になっているので、ここで入れる必要はない。

--- input/RtmpInput/src/main/java/com/ttProject/ozouni/rtmpInput/ReceiveWriter.java
+++ input/RtmpInput/src/main/java/com/ttProject/ozouni/rtmpInput/ReceiveWriter.java
@@ -8,18 +8,12 @@ package com.ttProject.ozouni.rtmpInput;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import com.flazr.rtmp.RtmpMessage;
 import com.flazr.rtmp.RtmpWriter;
 import com.ttProject.container.flv.FlvTag;
-import com.ttProject.container.flv.type.AudioTag;
-import com.ttProject.container.flv.type.VideoTag;
 import com.ttProject.flazr.unit.MessageManager;
-import com.ttProject.ozouni.base.ReportData;
 import com.ttProject.ozouni.base.worker.SendFrameWorker;
-import com.ttProject.ozouni.base.worker.SignalWorker;
-import com.ttProject.ozouni.dataHandler.ISendDataHandler;
 import com.ttProject.ozouni.rtmpInput.model.FlvTagOrderModel;
 import com.ttProject.ozouni.rtmpInput.model.IFlvTagOrderModel;
 
@@ -41,8 +35,8 @@ public class ReceiveWriter implements RtmpWriter {
 	/** データ共有用のworker */
 	private SendFrameWorker sendFrameWorker = null;
 	/** signalWorker外部から設定しておきます */
-	@Autowired
-	private SignalWorker signalWorker;
+//	@Autowired
+//	private SignalWorker signalWorker;
 	/**
 	 * publish通知をうけとったときの処理
 	 */
@@ -68,28 +62,28 @@ public class ReceiveWriter implements RtmpWriter {
 			FlvTag tag = messageManager.getTag(message);
 			orderModel.addTag(tag);
 			// audioTagのコーデック情報(サンプル数等も含めて)が変更になった場合等にffmpegとかの変換の場合は作り直す必要がある。
-			long pts = 0;
+//			ReportData reportData = signalWorker.getReportData();
+//			long pts = reportData.getFramePts();
 			for(FlvTag t : orderModel.getAudioCompleteTag()) {
 				logger.info("atag:{}", t);
 				// できたデータを登録しておく。
-				if(pts < t.getPts()) {
-					pts = t.getPts();
-				}
+//				if(pts < t.getPts()) {
+//					pts = t.getPts();
+//				}
 			}
 			for(FlvTag t : orderModel.getVideoCompleteTag()) {
 				logger.info("vtag:{}", t);
 				// こっちもできたデータを登録しておく。
-				if(pts < t.getPts()) {
-					pts = t.getPts();
-				}
+//				if(pts < t.getPts()) {
+//					pts = t.getPts();
+//				}
 			}
-			// とりあえずここにいれるのはおかしいだろう。
-			ReportData reportData = signalWorker.getReportData();
+/*			// とりあえずここにいれるのはおかしいだろう。
 			reportData.setLastUpdateTime(System.currentTimeMillis());
 			if(pts == 0) {
 				return;
 			}
-			reportData.setFramePts(pts);
+			reportData.setFramePts(pts);*/
 		}
 		catch(Exception e) {
 			logger.error("例外発生", e);
