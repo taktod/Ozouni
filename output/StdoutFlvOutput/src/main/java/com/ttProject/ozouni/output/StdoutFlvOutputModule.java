ae53cf5dc361646eb44d4e329d61186780863723
todatakahiko <toda.takahiko@hpsys.co.jp>
2014-07-25 16:47:57 +0900

    とりあえず、標準出力すると扱いにくいので、file出力にして動作テストしてみました。 きちんと動作するflvにすることができました。

--- output/StdoutFlvOutput/src/main/java/com/ttProject/ozouni/output/StdoutFlvOutputModule.java
+++ output/StdoutFlvOutput/src/main/java/com/ttProject/ozouni/output/StdoutFlvOutputModule.java
@@ -1,7 +1,11 @@
 package com.ttProject.ozouni.output;
 
+import java.nio.channels.Channels;
+
 import org.apache.log4j.Logger;
 
+import com.ttProject.container.flv.FlvHeaderTag;
+import com.ttProject.container.flv.FlvTagWriter;
 import com.ttProject.frame.IFrame;
 import com.ttProject.ozouni.base.IOutputModule;
 
@@ -12,8 +16,22 @@ import com.ttProject.ozouni.base.IOutputModule;
 public class StdoutFlvOutputModule implements IOutputModule {
 	/** ロガー */
 	private Logger logger = Logger.getLogger(StdoutFlvOutputModule.class);
+	private final FlvTagWriter writer;
+	/**
+	 * コンストラクタ
+	 */
+	public StdoutFlvOutputModule() throws Exception {
+		// 標準出力としてデータを出力するwriter
+//		writer = new FlvTagWriter(Channels.newChannel(System.out));
+		writer = new FlvTagWriter("hogehoge.flv");
+		FlvHeaderTag headerTag = new FlvHeaderTag();
+		headerTag.setAudioFlag(false);
+		headerTag.setVideoFlag(true);
+		writer.addContainer(headerTag);
+;	}
 	@Override
 	public void pushFrame(IFrame frame, int id) throws Exception {
 		logger.info(frame);
+		writer.addFrame(id, frame);
 	}
 }
