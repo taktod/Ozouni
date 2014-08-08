b09dca6b22208541351aa5f193c5e6bbccfcf29e
戸田貴彦 <toda@fc2.co.jp>
2014-08-08 14:05:53 +0900

    convertWorkModuleの内容を、namedPipeで動作するようにとりあえずやってみました。
    あとで、myLibにnamedPipeをつかって、変換するものをつくって、切り分けやっておきたいところ。

--- work/ConvertWork/src/main/java/com/ttProject/ozouni/work/ConvertWorkModule.java
+++ work/ConvertWork/src/main/java/com/ttProject/ozouni/work/ConvertWorkModule.java
@@ -1,22 +1,23 @@
 package com.ttProject.ozouni.work;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.channels.Channels;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import org.apache.log4j.Logger;
 
 import com.ttProject.container.IContainer;
-import com.ttProject.container.IReader;
+import com.ttProject.container.flv.FlvHeaderTag;
+import com.ttProject.container.flv.FlvTagWriter;
 import com.ttProject.container.mkv.MkvBlockTag;
 import com.ttProject.container.mkv.MkvTagReader;
-import com.ttProject.convertprocess.ProcessHandler;
-import com.ttProject.convertprocess.ProcessManager;
-import com.ttProject.convertprocess.process.FlvAudioOutputEntry;
-import com.ttProject.convertprocess.process.FlvOutputEntry;
-import com.ttProject.convertprocess.process.FlvVideoOutputEntry;
+import com.ttProject.frame.IAudioFrame;
 import com.ttProject.frame.IFrame;
-import com.ttProject.frame.extra.AudioMultiFrame;
+import com.ttProject.frame.IVideoFrame;
 import com.ttProject.nio.channels.IReadChannel;
+import com.ttProject.nio.channels.ReadableByteReadChannel;
 import com.ttProject.ozouni.base.IWorkModule;
 
 /**
@@ -27,104 +28,131 @@ import com.ttProject.ozouni.base.IWorkModule;
 public class ConvertWorkModule implements IWorkModule {
 	/** ロガー */
 	private Logger logger = Logger.getLogger(ConvertWorkModule.class);
-	/** 動作プロセスマネージャー */
-	private ProcessManager manager;
-	/** 動作開始フラグ */
 	private boolean startFlg = false;
-	/**
-	 * コンストラクタ
-	 */
-	public ConvertWorkModule() throws Exception {
-		try {
-			manager = new ProcessManager();
-		}
-		catch(Exception e) {
-			logger.error("例外が発生しました。", e);
-		}
-	}
+	private FlvTagWriter audioWriter = null;
+	private FlvTagWriter videoWriter = null;
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public synchronized void pushFrame(IFrame frame, int id) throws Exception {
-		// この部分で単体フレームがくるとは限らないので、multiFrame対策やっといた方がいいと思う。
 		if(!startFlg) {
-			// 開始処理を実施する
-			// 必要なプロセスをつくっておく。
-			// プロセスからデータを取り出したときの動作をつくっておく。
-			final ProcessHandler videoHandler = manager.getProcessHandler("video");
-			final ProcessHandler audioHandler = manager.getProcessHandler("audio");
-			videoHandler.setTargetClass(FlvVideoOutputEntry.class);
-			videoHandler.setCommand("/usr/local/bin/avconv -copyts -i - -vcodec mjpeg -s 160x120 -g 10 -q 20 -f matroska - 2>ffmpeg.video.log");
-			audioHandler.setTargetClass(FlvAudioOutputEntry.class);
-			audioHandler.setCommand("/usr/local/bin/avconv -copyts -i - -acodec adpcm_ima_wav -ac 1 -ar 22050 -f matroska - 2>ffmpeg.audio.log");
-/*			final ProcessHandler handler = manager.getProcessHandler("all");
-			handler.setTargetClass(FlvOutputEntry.class);
-			handler.setCommand("/usr/local/bin/avconv -copyts -i - acodec adpcm_ima_wav -ac 1 -ar 22050 -vcodec mjpeg -s 160x120 -g 10 -q 20 -f matroska - 2>ffmpeg.log");*/
-			manager.start(); // 動作開始
-			ExecutorService executor = Executors.newFixedThreadPool(2);
-			Thread.sleep(1000);
-			// 映像の出力結果を受け取る
-			executor.execute(new Runnable() {
-				@Override
-				public void run() {
-					try {
-						IReadChannel channel = videoHandler.getReadChannel();
-						IReader reader = new MkvTagReader();
-						IContainer container = null;
-						while((container = reader.read(channel)) != null) {
-							if(container instanceof MkvBlockTag) {
-								MkvBlockTag blockTag = (MkvBlockTag)container;
-								logger.info(blockTag.getFrame());
-							}
-						}
-					}
-					catch(Exception e) {
-					}
-				}
-			});
-			// 音声の出力結果を受け取る
-			executor.execute(new Runnable() {
-				@Override
-				public void run() {
-					try {
-						IReadChannel channel = videoHandler.getReadChannel();
-						IReader reader = new MkvTagReader();
-						IContainer container = null;
-						while((container = reader.read(channel)) != null) {
-							if(container instanceof MkvBlockTag) {
-								MkvBlockTag blockTag = (MkvBlockTag)container;
-								logger.info(blockTag.getFrame());
-							}
-						}
-					}
-					catch(Exception e) {
-					}
-				}
-			});// */
-/*			executor.execute(new Runnable() {
-				@Override
-				public void run() {
-					try {
-						IReadChannel channel = handler.getReadChannel();
-						IReader reader = new MkvTagReader();
-						IContainer container = null;
-						while((container = reader.read(channel)) != null) {
-							if(container instanceof MkvBlockTag) {
-								MkvBlockTag blockTag = (MkvBlockTag)container;
-								logger.info(blockTag.getFrame());
-							}
-						}
-					}
-					catch(Exception e) {
-					}
-				}
-			});// */
+			initialize();
 			startFlg = true;
 		}
-//		logger.info(frame);
-		// とりあえずここまでデータをもってくることはできた。
-		// flvデータをmkvデータに変換して動作させてみます。
-		manager.pushFrame(frame, id);
+		// その他いろいろやっておく。
+		if(frame instanceof IVideoFrame) {
+			try {
+				if(videoWriter != null) {
+					videoWriter.addFrame(id, frame);
+				}
+			}
+			catch(IOException e) {
+				logger.info("videopipeが壊れました。", e);
+			}
+		}
+		else if(frame instanceof IAudioFrame) {
+			try {
+				if(audioWriter != null) {
+					audioWriter.addFrame(id, frame);
+				}
+			}
+			catch(IOException e) {
+				logger.info("audiopipeが壊れました。", e);
+			}
+		}
+	}
+	private void initialize() throws Exception {
+		// ここの処理をつくっていきます。
+		makeNamedPipes();
+		// 音声は、無音frameを挿入して、なんとかしておく。(これはあとで考えるようにしておこう。)
+		// 映像は映像だけ、あるだけつくる感じにしておく。
+		// processBuilderを利用して、出力に関しては、このプログラムでうけとっておく。
+		// processBuilderをつくって、出力を取り出せるようにしておく
+		makeConvertProcesses();
+		
+		// flv出力なので、flv出力をつくっておく。
+		setupFlvWriters();
+	}
+	private void setupFlvWriters() throws Exception {
+		audioWriter = new FlvTagWriter("audio");
+		videoWriter = new FlvTagWriter("video");
+		FlvHeaderTag audioHeaderTag = new FlvHeaderTag();
+		audioHeaderTag.setAudioFlag(true);
+		audioHeaderTag.setVideoFlag(false);
+		audioWriter.addContainer(audioHeaderTag);
+		FlvHeaderTag videoHeaderTag = new FlvHeaderTag();
+		videoHeaderTag.setAudioFlag(false);
+		videoHeaderTag.setVideoFlag(true);
+		videoWriter.addContainer(videoHeaderTag);
+		// ここまででheaderの書き込みOK
+	}
+	private void makeConvertProcesses() throws Exception {
+		// 音声
+		StringBuilder audioCommand = new StringBuilder();
+		audioCommand.append("avconv -copyts -i audio -acodec adpcm_ima_wav -ar 22050 -ac 1 -f matroska -");
+		ProcessBuilder audioProcessBuilder = new ProcessBuilder("/bin/bash", "-c", audioCommand.toString());
+		audioProcessBuilder.environment().put("LD_LIBRARY_PATH", "/usr/local/lib");
+		// 映像
+		StringBuilder videoCommand = new StringBuilder();
+		videoCommand.append("avconv -copyts -i video -vcodec mjpeg -q 10 -r 10 -s 160x120 -f matroska -");
+		ProcessBuilder videoProcessBuilder = new ProcessBuilder("/bin/bash", "-c", videoCommand.toString());
+		videoProcessBuilder.environment().put("LD_LIBRARY_PATH", "/usr/local/lib");
+
+		final Process audioProcess = audioProcessBuilder.start();
+		final Process videoProcess = videoProcessBuilder.start();
+		ExecutorService ex = Executors.newCachedThreadPool();
+		ex.execute(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					IReadChannel channel = new ReadableByteReadChannel(Channels.newChannel(audioProcess.getInputStream()));
+					MkvTagReader reader = new MkvTagReader();
+					IContainer container = null;
+					while((container = reader.read(channel)) != null) {
+						if(container instanceof MkvBlockTag) {
+							MkvBlockTag blockTag = (MkvBlockTag)container;
+							IFrame frame = blockTag.getFrame();
+							logger.info(frame + ":" + frame.getPts() + "/" + frame.getTimebase());
+						}
+					}
+				}
+				catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		ex.execute(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					IReadChannel channel = new ReadableByteReadChannel(Channels.newChannel(videoProcess.getInputStream()));
+					MkvTagReader reader = new MkvTagReader();
+					IContainer container = null;
+					while((container = reader.read(channel)) != null) {
+						if(container instanceof MkvBlockTag) {
+							MkvBlockTag blockTag = (MkvBlockTag)container;
+							IFrame frame = blockTag.getFrame();
+							logger.info(frame + ":" + frame.getPts() + "/" + frame.getTimebase());
+						}
+					}
+				}
+				catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	private void makeNamedPipes() throws Exception {
+		namedPipe("audio");
+		namedPipe("video");
+	}
+	private void namedPipe(String name) throws Exception {
+		new File(name).delete();
+		StringBuilder command = new StringBuilder();
+		command.append("mkfifo " + name);
+		ProcessBuilder builder = new ProcessBuilder("/bin/bash", "-c", command.toString());
+		Process p = builder.start();
+		p.waitFor();
 	}
 }
