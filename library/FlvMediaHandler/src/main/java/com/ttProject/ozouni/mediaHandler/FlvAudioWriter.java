2afd483ac1af8c0e8317f45036207b7d644b086b
todatakahiko <toda.takahiko@hpsys.co.jp>
2014-08-27 14:21:34 +0900

    作成途上のデータをいったんコミットしておく。

---
+++ library/FlvMediaHandler/src/main/java/com/ttProject/ozouni/mediaHandler/FlvAudioWriter.java
@@ -0,0 +1,28 @@
+package com.ttProject.ozouni.mediaHandler;
+
+import com.ttProject.container.IContainer;
+import com.ttProject.container.IWriter;
+import com.ttProject.frame.IFrame;
+
+/**
+ * flvの出力を実施します。
+ * @author taktod
+ */
+public class FlvAudioWriter implements IWriter {
+	@Override
+	public void addContainer(IContainer container) throws Exception {
+		
+	}
+	@Override
+	public void addFrame(int trackId, IFrame frame) throws Exception {
+		
+	}
+	@Override
+	public void prepareHeader() throws Exception {
+		
+	}
+	@Override
+	public void prepareTailer() throws Exception {
+		
+	}
+}
---
+++ work/FfmpegWork/src/main/java/com/ttProject/ozouni/work/FfmpegVideoWorkModule.java
@@ -0,0 +1,5 @@
+package com.ttProject.ozouni.work;
+
+public class FfmpegVideoWorkModule {
+
+}
