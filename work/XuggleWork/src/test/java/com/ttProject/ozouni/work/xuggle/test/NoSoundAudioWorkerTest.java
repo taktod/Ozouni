3674ece9cae6abb57fbcd0bb808257fecdf65f2e
todatakahiko <toda.takahiko@hpsys.co.jp>
2014-08-28 06:17:40 +0900

    無音frameをつくるのにちょっと重宝している。 もうちょっと作りやすいプログラムをつくっておこうかな

--- work/XuggleWork/src/test/java/com/ttProject/ozouni/work/xuggle/test/NoSoundAudioWorkerTest.java
+++ work/XuggleWork/src/test/java/com/ttProject/ozouni/work/xuggle/test/NoSoundAudioWorkerTest.java
@@ -7,6 +7,7 @@ import com.ttProject.frame.IFrame;
 import com.ttProject.frame.aac.AacFrame;
 import com.ttProject.ozouni.base.IWorkModule;
 import com.ttProject.ozouni.work.XuggleAudioWorkModule;
+import com.ttProject.util.HexUtil;
 
 /**
  * 音声フレームが定期的にぬけている場合のaudioWorkerの動作テスト
@@ -25,29 +26,31 @@ public class NoSoundAudioWorkerTest {
 	public void test() throws Exception {
 		audioWorkModule = new XuggleAudioWorkModule();
 		audioWorkModule.setId(8);
-		audioWorkModule.setCodec("CODEC_ID_ADPCM_IMA_WAV");
+		audioWorkModule.setCodec("CODEC_ID_NELLYMOSER");
+		audioWorkModule.setSampleRate(44100);
+		audioWorkModule.setChannels(1);
+		audioWorkModule.setBitRate(96000);
 		audioWorkModule.setWorkModule(new IWorkModule() {
 			@Override
 			public void setWorkModule(IWorkModule workModule) {
 			}
 			@Override
-			public void pushFrame(IFrame frame, int id) throws Exception {
+			public synchronized void pushFrame(IFrame frame, int id) throws Exception {
+				logger.info("ここから");
 				logger.info(frame.getCodecType() + " " + frame.getPts() + " / " + frame.getTimebase());
+				logger.info(HexUtil.toHex(frame.getData()));
+				logger.info("ここまで");
 			}
 		});
 		logger.info("テスト開始");
 		AacFrame frame = null;
-//		NellymoserFrame nFrame = null;
 		for(int i = 0;i < 100;i ++) {
 			frame = AacFrame.getMutedFrame(44100, 1, 16);
 			frame.setPts(1000 * i);
 			frame.setTimebase(1000);
 			audioWorkModule.pushFrame(frame, 8);
-/*			nFrame = NellymoserFrame.getMutedFrame(22050, 1, 16);
-			nFrame.setPts(1000 * i + 500);
-			nFrame.setTimebase(1000);
-			audioWorkerModule.pushFrame(nFrame, 8);*/
 		}
 		// 本当はここでexecutorsから処理がおわるまで待機しないと最後まで処理したことにならないけど・・・
+		Thread.sleep(1000);
 	}
 }
